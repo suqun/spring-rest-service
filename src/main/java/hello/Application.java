ae11bdd5bce9bed328c2c4e9879ca733e4ea45c5
suqun <suqun@j1.com>
2015-09-02 14:15:48 +0800

    rest service

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Larry on 2015/9/2.
+ *
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
